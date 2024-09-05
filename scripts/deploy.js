748b3140f3732a7cf26c113bee174f0b9045a6b8
Hitesh2804 <hitesh.mittal@joinclamp.com>
2024-09-05 23:41:36 +0530

    refactor: some changes in script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,6 +17,15 @@ async function main() {
   ]);
   await Create2.waitForDeployment();
   console.log("contract Address:", Create2.target);
+
+  const Create3 = await hre.ethers.deployContract("BanterFantasySports", [
+    Create.target,
+    Create2.target,
+    "0xCA53DeeD55e5309350F5B242eE74531E853dBf20",
+  ]);
+  await Create3.waitForDeployment();
+
+  console.log("contract Address:", Create3.target);
 }
 
 main().catch((error) => {
