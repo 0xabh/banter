bd704f520f9d392808ad97d134f9649fb9451e71
Hitesh2804 <hitesh.mittal@joinclamp.com>
2024-09-05 13:32:12 +0530

    feat: creation of pools for added player tokens

--- contracts/TransferMarket.sol
+++ contracts/TransferMarket.sol
@@ -2,18 +2,59 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract PlayerTokenAMM {
+    using SafeERC20 for IERC20;
+
     address public owner;
     IERC20 public baseToken;
+    struct Pool {
+        uint256 baseTokenReserve;
+        uint256 playerTokenReserve;
+    }
+
+    mapping(address => Pool) public pools;
+    address[] public playerTokens;
+
+    event PoolCreated(address indexed playerToken);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
+
     constructor(address _baseToken, address _owner) {
         baseToken = IERC20(_baseToken);
         owner = _owner;
+    }
+
+    function createPool(
+        address _playerToken,
+        uint256 _initialLiquidity
+    ) external onlyOwner {
+        require(
+            pools[_playerToken].baseTokenReserve == 0,
+            "Pool already exists"
+        );
+
+        IERC20 playerToken = IERC20(_playerToken);
+
+        uint256 baseTokenAmount = _initialLiquidity;
+        uint256 playerTokenAmount = _initialLiquidity;
+
+        baseToken.safeTransferFrom(msg.sender, address(this), baseTokenAmount);
+        playerToken.safeTransferFrom(
+            msg.sender,
+            address(this),
+            playerTokenAmount
+        );
+
+        Pool storage newPool = pools[_playerToken];
+        newPool.baseTokenReserve = baseTokenAmount;
+        newPool.playerTokenReserve = playerTokenAmount;
+        playerTokens.push(_playerToken);
+
+        emit PoolCreated(_playerToken);
     }
 }
