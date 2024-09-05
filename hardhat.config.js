597ad7071c569b82c7abfad42b388c8aa57e878c
Hitesh2804 <hitesh.mittal@joinclamp.com>
2024-09-05 21:40:38 +0530

    added network configs of chiliz spicy testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    Chiliz: {
+      url: "https://chiliz-spicy.publicnode.com/",
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
