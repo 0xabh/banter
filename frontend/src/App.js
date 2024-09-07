acc158561322d7b2bdb262c070dca82ef47272d1
Hitesh2804 <hitesh.mittal@joinclamp.com>
2024-09-07 22:02:54 +0530

    minor changes/added toasts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import { ethers } from 'ethers';
-import './App.css';
-import { baseTokenContractAddress, getMainContract, getMainContractRead, getTokenAMMContract, getTokenContract, getWeb3Provider, requestAccounts, switchNetwork } from './Web3/web3';
-
+import { ethers } from "ethers";
+import "./App.css";
+import {
+  baseTokenContractAddress,
+  getMainContract,
+  getMainContractRead,
+  getTokenAMMContract,
+  getTokenContract,
+  getWeb3Provider,
+  requestAccounts,
+  switchNetwork,
+} from "./Web3/web3";
+import NavbarComponent from "./components/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import Players from "./components/Players";
+import Router from "./Routes/router";
 function App() {
   const connectWallet = async () => {
     if (!window.ethereum) {
@@ -17,68 +29,74 @@ function App() {
       alert(error);
     }
   };
-  const mintBaseToken=async()=>{
-    const provider=getWeb3Provider();
-    const contract=getTokenContract(provider,baseTokenContractAddress);
+  const mintBaseToken = async () => {
+    const provider = getWeb3Provider();
+    const contract = getTokenContract(provider, baseTokenContractAddress);
     console.log(contract);
-    const tx=await contract.mint("0x7783Eb57994CA3D2d94c56bdb7b4511e259Ab1b0",10000000);
+    const tx = await contract.mint(
+      "0x7783Eb57994CA3D2d94c56bdb7b4511e259Ab1b0",
+      10000000
+    );
     await tx.wait();
-  }
-  const setNewOwner=async()=>{
-    const provider=getWeb3Provider();
-    const contract=getTokenAMMContract(provider);
-    const tx=await contract.setNewOwner("0x7783Eb57994CA3D2d94c56bdb7b4511e259Ab1b0");
+  };
+  const setNewOwner = async () => {
+    const provider = getWeb3Provider();
+    const contract = getTokenAMMContract(provider);
+    const tx = await contract.setNewOwner(
+      "0x7783Eb57994CA3D2d94c56bdb7b4511e259Ab1b0"
+    );
     await tx.wait();
-  }
+  };
 
-  const addPLayer=async()=>{
-    const provider=getWeb3Provider();
+  const addPLayer = async () => {
+    const provider = getWeb3Provider();
     const signer = provider.getSigner();
-    const contractRead=getMainContractRead(provider);
-    const contract=contractRead.connect(signer);
-    contractRead.on("PlayerAdded",(tokenAddress, name, position,team,price) => {
-      console.log(tokenAddress);
-      console.log(name);
-    });
+    const contractRead = getMainContractRead(provider);
+    const contract = contractRead.connect(signer);
+    contractRead.on(
+      "PlayerAdded",
+      (tokenAddress, name, position, team, price) => {
+        console.log(tokenAddress);
+        console.log(name);
+      }
+    );
     console.log(contract);
-    const tx=await contract.addPlayer("Ronaldo","RON",1,1,2,10000,1000,{gasLimit:ethers.utils.hexlify(3000000)});
+    const tx = await contract.addPlayer("Messi", "MES", 1, 1, 3, 10000, 1000, {
+      gasLimit: ethers.utils.hexlify(3000000),
+    });
     await tx.wait();
-  }
-  const getOwner=async()=>{
-    const provider=getWeb3Provider();
-    const contract=getMainContractRead(provider);
-    const tx=await contract.owner();
+  };
+  const getOwner = async () => {
+    const provider = getWeb3Provider();
+    const contract = getMainContractRead(provider);
+    const tx = await contract.owner();
     console.log(tx);
-  }
-  const createTeam=async()=>{
-    const players=[""];
-    const leagueId= 1;
-    const provider=getWeb3Provider();
-    const contract=getMainContract(provider);
+  };
+  const createTeam = async () => {
+    const players = [""];
+    const leagueId = 1;
+    const provider = getWeb3Provider();
+    const contract = getMainContract(provider);
     console.log(contract);
     const amount = ethers.utils.parseEther("10");
-    const tx=await contract.createTeam(players,leagueId,{value:amount});
+    const tx = await contract.createTeam(players, leagueId, { value: amount });
     await tx.wait();
-  }
+  };
 
-  const createLeague=async()=>{
-    const provider=getWeb3Provider();
-    const contract=getMainContract(provider);
+  const createLeague = async () => {
+    const provider = getWeb3Provider();
+    const contract = getMainContract(provider);
     console.log(contract);
-    const tx=await contract.createLeagues("LALIGA",100000000,{gasLimit: ethers.utils.hexlify(3000000)});
+    const tx = await contract.createLeagues("LALIGA", 10000000000, {
+      gasLimit: ethers.utils.hexlify(3000000),
+    });
     await tx.wait();
-  }
-
-
+  };
 
   return (
     <div className="App">
-      <button onClick={connectWallet}>Connect</button>
-      <button onClick={mintBaseToken}>Mint</button>
-      <button onClick={setNewOwner}>Set</button>
-      <button onClick={addPLayer}>Add</button>
-      <button onClick={getOwner}>owner</button>
-      <button onClick={createLeague}>create League</button>
+      <NavbarComponent />
+      <Router />
     </div>
   );
 }
